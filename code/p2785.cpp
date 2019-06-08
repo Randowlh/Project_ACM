5519757c9ee6fd7aad83dd2c11410e674e878116
randow <1303775690@qq.com>
2019-06-08 19:03:50 +0800

    back up

---
+++ code/p2785.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+	int a[3];
+	cin>>a[0]>>a[1]>>a[2];
+	sort(a,a+3);
+	cout<<a[0]<<"->"<<a[1]<<"->"<<a[2]<<endl;
+	return 0;
+}
+

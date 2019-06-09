1311a08e071a4523921ccad0edbf21790a05c48e
randowlh <1303775690@qq.com>
2019-06-09 22:02:12 +0800

    2019/6/9 AC

---
+++ code/p1422.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+const double f=0.4463;
+const double s=0.4663;
+const double t=0.5663;
+int main(){
+  int a;
+  double ans;
+  scanf("%d",&a);
+  if(a<=150){
+    ans = a*f;
+  }else if(a<=400){
+    ans = (a-150)*s+150*f;
+  }else {
+    ans = (a-400)*t+250*s+150*f;
+  }
+  printf("%.1lf",ans);
+  return 0;
+
+}

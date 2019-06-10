e807a6a1aab48cdeee521aa54a5bed5ebfe40edf
randowlh <1303775690@qq.com>
2019-06-10 16:35:30 +0800

    2019/6/29 AC

--- code/p1089.cpp
+++ code/p1089.cpp
@@ -6,15 +6,14 @@ int main(){
   }
   int mom=0;
   int hand=0;
-  for(int i=0;i<=12;i++){
+  for(int i=0;i<12;i++){
     hand+=300;
     if(hand<a[i]){
       printf("-%d",i+1);
       return 0;
     }
     mom+=(hand-a[i])/100*100;
-    hand=hand-(hand-a[i])/100*100-a[i];
-    printf("%d\n",hand );
+    hand=hand-((hand-a[i])/100*100)-a[i];
   }
   printf("%d\n",mom/5*6+hand);
   return 0;

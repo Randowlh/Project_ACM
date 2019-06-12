002368920ca1df8cc791047e0e39cd643b85b649
randowlh <1303775690@qq.com>
2019-06-12 11:21:25 +0800

    2019/6/12 AC

---
+++ code/p1567.cpp
@@ -0,0 +1,23 @@
+#include<cstdio>
+int main(){
+  int ans=1;
+  int tmp=0;
+  int a=0;
+  int n;
+  int b=-1;
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+    scanf("%d",&a);
+    if(a>b){
+      tmp++;
+    }
+    else{
+      if(ans<tmp)
+      ans=tmp;
+      tmp=1;
+    }
+    b=a;
+  }
+  printf("%d\n",ans );
+  return 0;
+}

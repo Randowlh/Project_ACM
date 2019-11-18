2e8196ff93e05dbebf90e2a800cdac77b7dbcbe7
randowlh <1303775690@qq.com>
2019-11-18 15:55:46 +0800

    back up

--- code/homeworks/acmlesson7/2.cpp
+++ code/homeworks/acmlesson7/2.cpp
@@ -15,22 +15,24 @@ int ar[3];
 int main()
 {
     while (cin >> s >> a >> b)
-    {   if(s==0&&a==0&&b==0)
+    {
+        if (s == 0 && a == 0 && b == 0)
             break;
-        if(s%2!=0){
-        printf("NO\n");
-        continue;
-        }
-        head=0;
-        tail=0;
-        ar[0]=s;
-        ar[1]=a;
-        ar[2]=b;
-        for (int i = 0; i < s; i++)
+        if (s % 2 != 0)
         {
-            for (int j = 0; j < a; j++)
+            printf("NO\n");
+            continue;
+        }
+        head = 0;
+        tail = 0;
+        ar[0] = s;
+        ar[1] = a;
+        ar[2] = b;
+        for (int i = 0; i <= s; i++)
+        {
+            for (int j = 0; j <= a; j++)
             {
-                for (int k = 0; k < b; k++)
+                for (int k = 0; k <= b; k++)
                 {
                     dp[i][j][k] = -1;
                 }
@@ -42,16 +44,26 @@ int main()
         date[tail].a[2] = 0;
         date[tail].t = 0;
         tail++;
-        int ans=1<<29;
+        int ans = 1 << 29;
         while (tail ^ head)
-        {   
-            if (dp[date[head].a[0]][date[head].a[1]][date[head].a[2]] <= date[head].t&&dp[date[head].a[0]][date[head].a[1]][date[head].a[2]]!=-1)
-            {
+        {
+            if (dp[s / 2][s / 2][0] != -1 && s / 2 <= ar[1])
+                ans = dp[s / 2][s / 2][0];
+            if (dp[s / 2][s / 2][0] != -1 && s / 2 <= ar[1] && s / 2 <= ar[2])
+                ans = min(ans, dp[0][s / 2][s / 2]);
+            if (dp[s / 2][0][s / 2] != -1 && s / 2 <= ar[2])
+                ans = min(ans, dp[s / 2][0][s / 2]);
+            if(date[head].t>=ans){
+                head++;
+                continue;
+            }
+            if (dp[date[head].a[0]][date[head].a[1]][date[head].a[2]] <= date[head].t && dp[date[head].a[0]][date[head].a[1]][date[head].a[2]] != -1)
+            { //printf("%d %d %d\n",date[head].a[0],date[head].a[1],date[head].a[2]);
                 head++;
                 continue;
             }
             else
-            {
+            { // printf("%d %d %d\n",date[head].a[0],date[head].a[1],date[head].a[2]);
                 dp[date[head].a[0]][date[head].a[1]][date[head].a[2]] = date[head].t;
             }
             for (int i = 0; i < 4; i++)
@@ -66,7 +78,7 @@ int main()
                             {
                                 date[tail].a[j] = date[head].a[i] + date[head].a[j];
                                 date[tail].a[i] = 0;
-                                date[tail].a[(3-i-j)] = date[head].a[(3-i-j)];
+                                date[tail].a[(3 - i - j)] = date[head].a[(3 - i - j)];
                                 date[tail].t = date[head].t + 1;
                                 tail++;
                             }
@@ -74,7 +86,7 @@ int main()
                             {
                                 date[tail].a[i] = date[head].a[i] - ar[j] + date[head].a[j];
                                 date[tail].a[j] = ar[j];
-                                date[tail].a[(3-i-j)] = date[head].a[(3-i-j)];
+                                date[tail].a[(3 - i - j)] = date[head].a[(3 - i - j)];
                                 date[tail].t = date[head].t + 1;
                                 tail++;
                             }
@@ -84,15 +96,10 @@ int main()
             }
             head++;
         }
-        if(dp[s/2][s/2][0]!=-1&&s/2<=ar[1])
-        ans=dp[s/2][s/2][0];
-        if(dp[s/2][s/2][0]!=-1&&s/2<=ar[1]&&s/2<=ar[2])
-        ans=min(ans,dp[0][s/2][s/2]);
-        if(dp[s/2][0][s/2]!=-1&&s/2<=ar[2])
-        ans=min(ans,dp[s/2][0][s/2]);
-        if(ans!=1<<29)
-        printf("%d\n",ans);
-        else{
+        if (ans != 1 << 29)
+            printf("%d\n", ans);
+        else
+        {
             printf("NO\n");
         }
     }

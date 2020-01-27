37c05f92ea0a52b944f2db28045f0d1784ce4e5a
randowlh <1303775690@qq.com>
2020-01-27 23:21:36 +0800

    back up

---
+++ code/hdoj/2722.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+#define inf 10000000
+using namespace std;
+
+int mp[510][510];
+int flag[510], dis[510];
+int m, n;
+
+void dijkstra(int v)
+{
+    for (int i = 1; i <= n; i++)
+        dis[i] = mp[v][i];
+    flag[v] = 1;
+    while (1)
+    {
+        int min = inf, k = -1;
+        for (int i = 1; i <= n; i++)
+            if (!flag[i] && min > dis[i])
+            {
+                min = dis[i];
+                k = i;
+            }
+        if (k == -1)
+            break;
+        flag[k] = 1;
+        for (int i = 1; i <= n; i++)
+            if (!flag[i] && dis[i] > min + mp[k][i])
+                dis[i] = min + mp[k][i];
+    }
+}
+
+int main()
+{
+    while (scanf("%d%d", &n, &m)!=EOF)
+    {
+        if (n + m == 0)
+            break;
+        memset(flag, 0, sizeof(flag));
+        for (int i = 0; i < 510; i++)
+            for (int j = 0; j < 510; j++)
+                mp[i][j] = inf;
+        for (int i = 0; i < 510; i++)
+            mp[i][i] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                int x, a = (i - 1) * (m + 1) + j;
+                char dir;
+                scanf("%d %c", &x, &dir);
+                if (x == 0)
+                    continue;
+                if (dir == '>')
+                    mp[a][a + 1] = 2520 / x;
+                else if (dir == '<')
+                    mp[a + 1][a] = 2520 / x;
+                else if (dir == '*')
+                    mp[a][a + 1] = mp[a + 1][a] = 2520 / x;
+            }
+            for (int j = 1; j <= m + 1; j++)
+            {
+                int x, a = (i - 1) * (m + 1) + j;
+                char dir;
+                scanf("%d %c", &x, &dir);
+                if (x == 0)
+                    continue;
+                if (dir == 'v')
+                    mp[a][a + 1 + m] = 2520 / x;
+                else if (dir == '^')
+                    mp[a + 1 + m][a] = 2520 / x;
+                else if (dir == '*')
+                    mp[a][a + 1 + m] = mp[a + 1 + m][a] = 2520 / x;
+            }
+        }
+        for (int j = 1; j <= m; j++)
+        {
+            int x, a = n * (m + 1) + j;
+            char dir;
+            scanf("%d %c", &x, &dir);
+            if (x == 0)
+                continue;
+            if (dir == '>')
+                mp[a][a + 1] = 2520 / x;
+            else if (dir == '<')
+                mp[a + 1][a] = 2520 / x;
+            else if (dir == '*')
+                mp[a][a + 1] = mp[a + 1][a] = 2520 / x;
+        }
+        n = (n + 1) * (m + 1);
+        dijkstra(1);
+        if (dis[n] < inf)
+            printf("%d blips\n", dis[n]);
+        else
+            printf("Holiday\n");
+    }
+    return 0;
+}
--- code/hdoj/2923.cpp
+++ code/hdoj/2923.cpp
@@ -1,15 +1,87 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int inf =0x7FFFFFFF;
+const int inf = 10000000;
 typedef long long ll;
-void work(){
-
+int mp[100][100];
+int n,c,r;
+int cnt;
+map<string,int> M;
+void floyd()
+{
+    for (int k = 1; k <= n; k++)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                if (mp[i][j] > mp[i][k] + mp[k][j])
+                    mp[i][j] = mp[i][k] + mp[k][j];
 }
-int main(){
-    //freopen("in.txt","r",stdin);
-    int t=1;
-    cin>>t;
-    while(t--){
+int fenpei(string k){
+    if(M.count(k)==0){
+        M[k]=cnt;
+        cnt++;
+        return cnt-1;
+    }else 
+    return M[k];
+}
+void work()
+{
+    int k=0;
+    while(cin>>n>>c>>r){
+        M.clear();
+        cnt=2;
+        k++;
+        
+        if(n==0&&c==0&&r==0){
+            break;
+        }
+        cout<<k<<'.'<<' ';
+        for(int i=0;i<=n;i++)
+        for(int j=0;j<=n;j++){
+            mp[i][j]=inf;
+        }
+        for(int i=0;i<=n;i++)
+            mp[i][i]=0;
+        string tmp;
+        int start=1;
+        cin>>tmp;
+        M[tmp]=1;
+        vector<int> e;
+        for(int i=0;i<c;i++){
+            cin>>tmp;
+            e.push_back(fenpei(tmp));
+        }
+        char backw,forw,t;
+        int v;
+        string tmp2;
+        for(int i=0;i<r;i++){
+            cin>>tmp;
+            cin>>backw>>t>>v>>t>>forw;
+            cin>>tmp2;
+            int w=fenpei(tmp);
+            int u=fenpei(tmp2);
+            if(backw=='<'){
+                mp[u][w]=min(mp[u][w],v);
+            }
+            if(forw=='>'){
+                mp[w][u]=min(mp[w][u],v);
+            }
+        }
+        floyd();
+        ll ans=0;
+        for(int i=0;i<c;i++){
+            ans+=mp[start][e[i]];
+            ans+=mp[e[i]][start];
+        }
+        cout<<ans<<endl;
+    }
+    return ;
+}
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
         work();
     }
     return 0;

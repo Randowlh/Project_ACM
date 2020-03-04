2b165fbe3853deaba7177da3675d658ee3384597
randowlh <1303775690@qq.com>
2020-03-04 22:20:58 +0800

    back up

--- code/hdoj/ACM_steps/section_1/8.cpp
+++ code/hdoj/ACM_steps/section_1/8.cpp
@@ -1,19 +1,25 @@
-#include<cstdio>
-int main(){
-    int n,m;
-    int tmp,ans;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&m);
-        ans=0;
-        for(int j=0;j<m;j++){
-            scanf("%d",&tmp);
-            ans+=tmp;
+#include <cstdio>
+int main()
+{
+    int n, m;
+    int tmp, ans;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &m);
+        ans = 0;
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &tmp);
+            ans += tmp;
         }
-        printf("%d\n",ans);
-        if(i==n-1){
+        printf("%d\n", ans);
+        if (i == n - 1)
+        {
             continue;
-        }else{
+        }
+        else
+        {
             printf("\n");
         }
     }

df9b436f787396d73de93d8b8e66c7bf5762cc58
randowlh <1303775690@qq.com>
2020-03-09 17:17:01 +0800

    back up

--- code/hdoj/2048.cpp
+++ code/hdoj/2048.cpp
@@ -1,47 +1,27 @@
-#include<cstdio>
-#include<iostream>
-#include<algorithm>
-#include<cmath>
+#include <bits/stdc++.h>
 using namespace std;
-struct node
+double dp[100], chu[100];
+int main()
 {
-    int x;
-    int y;
-}date[110];
-double cx,cy;
-double tol=0;
-int rnd,n;
-double dis(double x1,double y1,double x2,double y2){
-    return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-}
-int main(){
-        tol=0;
-        while(scanf("%d",&n)!=EOF){
-            if(n==0){
-                break;
-            }
-            tol=0;
-            double tmpx=0;
-          double tmpy=0;
-          for(int i=0;i<n;i++){
-              scanf("%d%d",&date[i].x,&date[i].y);
-              tmpx+=date[i].x;
-              tmpy+=date[i].y;
-            }
-            cx=tmpx/n;
-            cy=tmpy/n;
-            double a,b,c,tmp;
-            date[n].x=date[0].x;
-            date[n].x=date[0].y;
-          for(int i=0;i<n;i++){
-              a=dis(date[i].x,date[i].y,cx,cy);
-              b=dis(date[i].x,date[i].y,date[i+1].x,date[i+1].y);
-              c=dis(date[i+1].x,date[i+1].y,cx,cy);
-              tmp=(a+b+c)/2;
-              tol+=sqrt(tmp*(tmp-a)*(tmp-b)*(tmp-c));
-          }
-            printf("%.1f\n",tol);     
-                
+    dp[1] = 0;
+    dp[2] = 1;
+    for (long long i = 3; i <= 20; i++)
+    {
+        dp[i] = (i - 1) * (dp[i - 1] + dp[i - 2]);
+    }
+    chu[1] = 1;
+    chu[2] = 2;
+    for (long long i = 3; i <= 30; i++)
+    {
+        chu[i] = chu[i - 1] * i;
+    }
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        printf("%.2f%%\n", (double)dp[n] * 100.0 / (double)chu[n]);
     }
     return 0;
 }

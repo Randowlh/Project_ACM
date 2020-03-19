62a213db52d272b9a08119017161c1e699805e5b
randowlh <1303775690@qq.com>
2020-03-19 22:24:24 +0800

    back up

--- code/competition/Educational Codeforces Round 83/4.cpp
+++ code/competition/Educational Codeforces Round 83/4.cpp
@@ -4,9 +4,41 @@ const int inf = 0x7FFFFFFF;
 #define bug puts("here\n")
 typedef long long ll;
 const int mod = 998244353;
-int dp[210000];
+ll F[200100], G[200100];
+ll pows(int i, ll a)
+{
+    ll ans = 1;
+    ll tmp = 1;
+    ll k = i;
+    while (a)
+    {
+        if (a & 1)
+        {
+            ans = (ans * k) % mod;
+        }
+        a >>= 1;
+        k = (k * k) % mod;
+    }
+    return ans;
+}
+void init()
+{
+    F[0] = 1;
+    G[0] = 1;
+    for (int i = 1; i <= 200010; i++)
+    {
+        F[i] = F[i - 1] * i % mod;
+        G[i] = G[i - 1] * pows(i, mod - 2) % mod;
+    }
+    return;
+}
+ll C(ll n, ll m)
+{
+    return F[n] * G[m] % mod * G[n - m] % mod;
+}
 void work()
 {
+    init();
     int n, m;
     cin >> n >> m;
     if (m < n - 1)
@@ -14,13 +46,25 @@ void work()
         cout << 0 << endl;
         return;
     }
-    dp[n - 1] =
+    ll ans = 0;
+    if (n <= 2)
+    {
+        cout << 0 << endl;
+        return;
+    }
+    for (int i = n - 2; i < m; i++)
+    {
+        ans = (C(i, n - 2) + ans) % mod;
+    }
+    ans = (ans * (n - 2)) % mod * pows(2, n - 3) % mod;
+    cout << ans << endl;
+    return;
 }
 int main()
 {
     std::ios::sync_with_stdio(false);
     cin.tie(NULL);
-    freopen("in.txt", "r", stdin);
+    // freopen("in.txt", "r", stdin);
     int t = 1;
     //cin>>t;
     while (t--)
---
+++ code/competition/Educational Codeforces Round 83/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+
+    //     tmp = C(i - 1, n - 2);
+    //     // cout << tmp << endl;

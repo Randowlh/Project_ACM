238ad3fa5e9bf3b3da2cf06377c26b250c97ad94
randowlh <1303775690@qq.com>
2020-04-08 22:06:31 +0800

    back up

--- code/competition/Codeforces Round #605/5.cpp
+++ code/competition/Codeforces Round #605/5.cpp
@@ -5,59 +5,59 @@ const int inf = 0x7FFFFFFF;
 typedef long long ll;
 const int mod = (0 ? 1000000007 : 998244353);
 const double eps = 1e-7;
-int dp[200000];
-int a[200000];
+int dp[200010];
+int a[200010];
+vector<int> mp[200010];
 int n;
-int dfs(int x, int fl = 0)
-{
-    if (dp[x] != 0 && !fl)
-        return dp[x];
-    if (dp[x] > 0)
-        return dp[x];
-    int ans = inf - 100;
-    dp[x] = -1;
-    if (x - a[x] >= 0)
-    {
-        if (a[x] & 1 != a[x - a[x]] & 1)
-            ans = 1;
-        else
-        {
-            int t = dfs(x - a[x]);
-            if (t != -1)
-                ans = min(ans, t + 1);
-        }
-    }
-    if (x + a[x] < n)
-    {
-        if (a[x] & 1 != a[x + a[x]] & 1)
-            ans = 1;
-        else
-        {
-            int t = dfs(x + a[x]);
-            if (t != -1)
-                ans = min(ans, t + 1);
-        }
-    }
-    if (ans == inf - 100)
-        ans = -1;
-    dp[x] = ans;
-    return ans;
-}
 void work()
 {
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
-        dp[i] = 0;
+        dp[i] = inf;
     }
     for (int i = 0; i < n; i++)
     {
-        dfs(i, 1);
+        if (i - a[i] >= 0 && a[i - a[i]] % 2 == a[i] % 2)
+            mp[i - a[i]].push_back(i);
+        if (i + a[i] < n && a[i + a[i]] % 2 == a[i] % 2)
+            mp[i + a[i]].push_back(i);
     }
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", dp[i]);
+        if (i - a[i] >= 0 && a[i - a[i]] % 2 != a[i] % 2)
+            dp[i] = 1;
+        if (i + a[i] < n && a[i + a[i]] % 2 != a[i] % 2)
+            dp[i] = 1;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (dp[i] == 1)
+        {
+            queue<pair<int, int>> q;
+            q.push(make_pair(1, i));
+            while (!q.empty())
+            {
+                pair<int, int> x = q.front();
+                q.pop();
+                for (int i = 0; i < mp[x.second].size(); i++)
+                {
+                    if (dp[mp[x.second][i]] > x.first + 1)
+                    {
+                        dp[mp[x.second][i]] = x.first + 1;
+                        q.push(make_pair(x.first + 1, mp[x.second][i]));
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (dp[i] == inf)
+            printf("-1 ");
+        else
+            printf("%d ", dp[i]);
     }
     printf("\n");
 }
@@ -65,7 +65,7 @@ signed main()
 {
     std::ios::sync_with_stdio(false);
     cin.tie(NULL);
-    freopen("in.txt", "r", stdin);
+    //freopen("in.txt", "r", stdin);
     int t = 1;
     //cin>>t;
     //1 2 1 1 1 1 1 2 2 1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 1 1 1 1 1 1 1 1 1 1 2 1 1 1 1 2 1 1 1 2 1 1 1 1 3 1 1 2 1 2 1 1 1 1 1 1 1 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1 2 1 2 1 2 1 2 1 1 1 3 1 3 2 3 2 2 1

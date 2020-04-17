a12893e9afffc7cf4bb39f633363e8ea4dd9f53b
randowlh <1303775690@qq.com>
2020-04-17 19:10:28 +0800

    back up

--- code/hdoj/1533.cpp
+++ code/hdoj/1533.cpp
@@ -1,5 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <queue>
+#include <cstring>
 using namespace std;
+#define int long long
 struct LCF
 {
     struct Edge
@@ -8,13 +12,13 @@ struct LCF
         int to;
         int flow;
         int cost;
-        Edge(int fr, int t, int f, int cc) : from(fr), to(t), flow(f), cost(cc) {}
+        Edge(int fr = 0, int t = 0, int f = 0, int cc = 0) : from(fr), to(t), flow(f), cost(cc) {}
     };
 #define inf 0x3f3f3f3f
-#define maxn 200010
+#define maxn 201110
     int book[maxn];
     int dis[maxn];
-    int n, m, s, t, ans = 0;
+    int n, m, s, t, ans;
     vector<Edge> edge;
     vector<int> mp[maxn];
     void add_edge(int x, int y, int flow, int cost)
@@ -24,9 +28,9 @@ struct LCF
         mp[x].push_back(edge.size() - 2);
         mp[y].push_back(edge.size() - 1);
     }
-    inline bool spfa()
+    bool spfa()
     {
-        for (int i = 0; i <= n; i++)
+        for (int i = 0; i <= n * m + 100; i++)
         {
             dis[i] = inf;
             book[i] = 0;
@@ -63,14 +67,12 @@ struct LCF
     {
         book[x] = 1;
         if (x == t)
-        {
             return flow;
-        }
         int used = 0, a;
         for (int i = 0; i < mp[x].size(); i++)
         {
             int to = edge[mp[x][i]].to;
-            if (!book[to] && edge[mp[x][i]].flow && dis[x] - edge[mp[x][i]].cost == dis[to])
+            if (!book[to] && edge[mp[x][i]].flow > 0 && dis[x] - edge[mp[x][i]].cost == dis[to])
             {
                 a = dfs(to, min(flow - used, edge[mp[x][i]].flow));
                 if (a)
@@ -81,34 +83,68 @@ struct LCF
         }
         return used;
     }
-    inline int cost_flow()
+    void init(int tn, int tm, int ts, int tt)
+    {
+        for (int i = 0; i <= tn * tm + 100; i++)
+            mp[i].clear();
+        edge.clear();
+        n = tn;
+        m = tm;
+        s = ts;
+        t = tt;
+        ans = 0;
+    }
+    int cost_flow()
     {
         int flow = 0;
         while (spfa())
         {
-            memset(book, 0, sizeof(book));
+            for (int i = 0; i <= n * m + 200; i++)
+                book[i] = 0;
             flow += dfs(s, inf);
         }
         return flow;
     }
 } tmp;
-int main()
+char mp[1100][1100];
+signed main()
 {
     std::ios::sync_with_stdio(false);
     cin.tie(NULL);
-    freopen("in.txt", "r", stdin);
-    cin >> tmp.n >> tmp.m >> tmp.s >> tmp.t;
-    int w, u, v, c;
-    tmp.edge.clear();
-    for (int i = 0; i <= tmp.n; i++)
+    //freopen("in.txt", "r", stdin);
+    int n = 0, m = 0;
+    while (cin >> n >> m)
     {
-        tmp.mp[i].clear();
+        if (n == 0 && m == 0)
+            break;
+        //memset(mp, 0, sizeof(mp));
+        tmp.init(n, m, 0, n * m + 7);
+        vector<int> hs, ps;
+        vector<pair<int, int>> lh, lp;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+            {
+                cin >> mp[i][j];
+                if (mp[i][j] == 'm')
+                {
+                    ps.push_back(i * n + j + 1);
+                    lh.push_back(make_pair(i, j));
+                }
+                else if (mp[i][j] == 'H')
+                {
+                    hs.push_back(i * n + j + 1);
+                    lp.push_back(make_pair(i, j));
+                }
+            }
+        for (int i = 0; i < ps.size(); i++)
+            tmp.add_edge(0, ps[i], 1, 0);
+        for (int i = 0; i < ps.size(); i++)
+            for (int j = 0; j < hs.size(); j++)
+                tmp.add_edge(ps[i], hs[j], 0x3f3f3f3f - 1, abs(lh[i].first - lp[j].first) + abs(lh[i].second - lp[j].second));
+        for (int i = 0; i < hs.size(); i++)
+            tmp.add_edge(hs[i], n * m + 7, 1, 0);
+        tmp.cost_flow();
+        cout << tmp.ans << endl;
     }
-    for (int i = 0; i < tmp.m; i++)
-    {
-        cin >> w >> u >> v >> c;
-        tmp.add_edge(w, u, v, c);
-    }
-    cout << tmp.cost_flow() << ' ' << tmp.ans << endl;
     return 0;
 }

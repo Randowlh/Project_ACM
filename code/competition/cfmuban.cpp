e624ee78935f8d1b6fe566e65fcaa576a4f6a50b
randowlh <1303775690@qq.com>
2020-04-30 23:12:37 +0800

    back up

--- code/competition/cfmuban.cpp
+++ code/competition/cfmuban.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int inf = 0x7FFFFFFF;
-#define bug cout << "here\n"
 typedef long long ll;
 #define int long long
 const int mod = (0 ? 1000000007 : 998244353);

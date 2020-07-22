2d735289a4f484230d7dc3fd1d773650aade058e
randowlh <1303775690@qq.com>
2020-07-22 18:54:12 +0800

    back up

--- code/luogu/5095.cpp
+++ code/luogu/5095.cpp
@@ -1,27 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-int ans = 0;
-pair<int, int> date[500050];
-int n;
-int l;
-int main(){
+#define int long long
+int n, l;
+int h[2010], w[2010];
+int prw[2010];
+int dp[2010];
+struct st_table
+{
+    int date[2010];
+    int mx[2010][12];
+    inline void RMQ(int num)
+    {
+        for (int i = 1; i <= num; i++)
+            mx[i][0] = date[i];
+        for (int j = 1; j < 20; ++j)
+            for (int i = 1; i <= num; ++i)
+                if (i + (1 << j) - 1 <= num)
+                    mx[i][j] = max(mx[i][j - 1], mx[i + (1 << (j - 1))][j - 1]);
+    }
+    inline int query(int l, int r)
+    {
+        int k = log(r - l + 1) / log(2);
+        return max(mx[l][k], mx[r - (1 << k) + 1][k]);
+    }
+} b;
+signed main(){
+    //freopen("in.txt", "r", stdin);
     cin >> n >> l;
-    for (int i = 0; i < n;i++){
-        cin>>date[i].first>>date[i].second;
-    }
-    int now = 0;
-    int tmp = 0;
-    int lb = 0;
-    while(lb<n){
-        if (date[lb].second + now >l){
-            ans += tmp;
-            now = date[lb].second;
-            tmp = date[lb].first;
-        }else{
-            now += date[lb].second;
-            tmp=max(tmp,date[lb].first);
+    for (int i = 1; i <= n;i++)
+        cin >> h[i] >> w[i],b.date[i]=h[i];
+    b.RMQ(n);
+    dp[0] = 0;
+    for (int i = 1; i <= n;i++)
+        prw[i]=prw[i-1]+w[i];
+    for (int i = 1; i <= n;i++){
+        dp[i] = dp[i - 1] + h[i];
+        for (int j = 0; j <= i-1;j++){
+            if(prw[i]-prw[j]<=l){
+                dp[i] = min(dp[i], dp[j]+ b.query(j+1 , i));
+            //    cout << "i=" << i << ' ' << j << endl;
+            }
         }
-        lb++;
     }
-    cout << ans << endl;
+    cout<<dp[n]<<endl;
 }   

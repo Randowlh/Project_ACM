04a24f82b0f764d0db1ee63c11c81a591602b76f
randowlh <1303775690@qq.com>
2020-10-05 23:27:48 +0800

    back up

--- code/homeworks/Date_Struct/2.cpp
+++ code/homeworks/Date_Struct/2.cpp
@@ -3,28 +3,30 @@
 using namespace std;
 int mp[110][110];
 int vis[110][110];
+int tvis[110][110];
 int b[][2]={{0,0},{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+int br[][2]={{0,0},{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 pair<int,int> q[11000];
 //⏩⏪⏫⏬
 int tail,head,flag;
-mt19937 rnd(233);
 int n;
 pair<int,int> s(1,1),e(20,20);
-mt19937 rnd(514114);
 template<class T> class fhq_treap
 {
+	public:
 	struct node
 	{
 		int l, r;
 		T val;
 		int key;
 		int size;
-	} fhq[10];
+	} ;
+	node fhq[11000];
 	int cnt, root;
-	inline int newnode(int val)
+	inline int newnode(T val)
 	{
 		fhq[++cnt].val = val;
-		fhq[cnt].key = rnd();
+		fhq[cnt].key = rand();
 		fhq[cnt].size = 1;
 		fhq[cnt].l = fhq[cnt].r = 0;
 		return cnt;
@@ -33,7 +35,7 @@ template<class T> class fhq_treap
 	{
 	fhq[now].size = fhq[fhq[now].l].size + fhq[fhq[now].r].size + 1;
 	}
-	void split(int now, int val, int &x, int &y)
+	void split(int now, T val, int &x, int &y)
 	{
 		if (!now)
 		{
@@ -69,16 +71,18 @@ template<class T> class fhq_treap
 			return y;
 		}
 	}
-	inline void insert(int val)
+	inline void insert(T val)
 	{
 		int x, y;
 		split(root, val, x, y);
 		root = merge(merge(x, newnode(val)), y);
 	}
-	inline void del(int val)
+	inline void del(T val)
 	{
 		int x, y, z;
-		split(root, val - 1, x, y);
+		val.second.second-=1;
+		split(root, val, x, y);
+		val.second.second+=1;
 		split(y, val, y, z);
 		y = merge(fhq[y].l, fhq[y].r);
 		root = merge(merge(x, y), z);
@@ -108,10 +112,12 @@ template<class T> class fhq_treap
 		}
 		return fhq[now].val;
 	}
-	inline int pre(int val)
+	inline T pre(T val)
 	{
-		int x, y, ans;
-		split(root, val - 1, x, y);
+		int x, y;
+		T ans;
+		val.second.second-=1;
+		split(root, val, x, y);
 		int t = x;
 		while (fhq[t].r)
 			t = fhq[t].r;
@@ -119,9 +125,10 @@ template<class T> class fhq_treap
 		root = merge(x, y);
 		return ans;
 	}
-	inline int aft(int val)
+	inline T aft(T val)
 	{
-		int x, y, ans;
+		int x, y;
+		T ans;
 		split(root, val, x, y);
 		int t = y;
 		while (fhq[t].l)
@@ -144,16 +151,18 @@ void flsh(){
 			if(!mp[i][j]){
 				if(!vis[i][j])
 					cout<<"⬛";
-				else cout<<vis[i][j];
-				// else if(vis[i][j]==1)
-				// 	cout<<"⏩";
-				// else if(vis[i][j]==2)
-				// 	cout<<"⏫";
-				// else if(vis[i][j]==3)
-				// 	cout<<"⏪";
-				// else if(vis[i][j]==4)
-				// 	cout<<"⏬";
-				// else cout<<"❌";
+				else if(vis[i][j]==1)
+					cout<<"⏬";
+				else if(vis[i][j]==2)
+					cout<<"⏩";
+				else if(vis[i][j]==3)
+					cout<<"⏫";
+				else if(vis[i][j]==4)
+					cout<<"⏪";
+				else if(vis[i][j]==6)
+                cout<<"⭐";
+                else 
+                cout<<"❌";
 			}
 			else cout<<"⬜";
 		}
@@ -192,7 +201,7 @@ void gen(){
 			mp[i][j]=1;
 	for(int i=1;i<=n;i++)
 		for(int j=1;j<=n;j++)
-			if(rnd()%10<5)
+			if(rand()%10<5)
 				mp[i][j]=0;
 			else mp[i][j]=1;
 		mp[1][1]=0;
@@ -208,7 +217,7 @@ void gen(){
 }
 void getfile(){
 	system("cls");
-	cout<<"Please your file name:";
+	cout<<"Please your file name:"<<flush;
 	string f;
 	cin>>f;
 	ifstream in(f);
@@ -225,21 +234,24 @@ void dfs(int x,int y){
 	system("cls");
 	if(x==n&&y==n){
 		cout<<"path find!"<<endl;
+        vis[n][n]=6;
 		flsh();
 		flag=1;
 		system("pause");
 		return;
 	}
 	cout<<"processing"<<endl;
-	cout<<"now="<<x<<' '<<y<<endl;
 	flsh();
 	Sleep(100);
-	set<int> s;
-	while(s.size()<4){
-		int i=rnd()%4+1;
-		if(s.count(i))
+	int t[5];
+	memset(t,0,sizeof(t));
+	int cnt=0;
+	while(!flag&&cnt<4){
+		int i=rand()%4+1;
+		if(t[i])
 			continue;
-		s.insert(i);
+		t[i]=1;
+		cnt++;
 		if(ck(x+b[i][0],y+b[i][1])){
 			vis[x][y]=i;
 			dfs(x+b[i][0],y+b[i][1]);
@@ -256,19 +268,93 @@ void show(){
 	flag=0;
 	dfs(1,1);
 }
+void srt(){
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n;j++)
+			vis[i][j]=0;
+	fhq_treap<pair<int,pair<int,int>>> q;
+	q.insert(make_pair(0,make_pair(1,1)));
+	vis[1][1]=-1;
+	pair<int,pair<int,int>> mi=make_pair(-999999,make_pair(1,1));
+	q.insert(mi);
+	q.insert(make_pair(999999,make_pair(1,1)));
+	while(q.fhq[q.root].size>2){
+		pair<int,pair<int,int>>t=q.aft(mi);
+		q.del(t);
+		for(int i=1;i<=4;i++){
+			if(ck(t.second.first+b[i][0],t.second.second+b[i][1])){
+				vis[t.second.first+b[i][0]][t.second.second+b[i][1]]=i;
+				q.insert(make_pair(t.first+1,make_pair(t.second.first+b[i][0],t.second.second+b[i][1])));
+			}
+		}
+	}
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n;j++)
+			tvis[i][j]=0;
+	int x=n,y=n,t,tx,ty;
+	while(vis[x][y]!=-1){
+		tx=x+br[vis[x][y]][0];
+		ty=y+br[vis[x][y]][1];
+		tvis[tx][ty]=vis[x][y];
+		x=tx,y=ty;
+	}
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n;j++)
+			vis[i][j]=tvis[i][j];
+	vis[n][n]=6;
+	system("cls");
+	cout<<"The shortest path is:"<<endl;
+	flsh();
+	system("pause");
+}
+void sdfs(int x,int y){
+	if(x==n&&y==n){
+		for(int i=0;i<tail-1;i++){
+			cout<<'{'<<q[i].first<<','<<q[i].second<<"},";
+		}
+		cout<<'{'<<n<<','<<n<<'}'<<endl;
+		return;
+	}
+	for(int i=1;i<=4;i++){
+		if(ck(x+b[i][0],y+b[i][1])){
+			q[tail++]=make_pair(x+b[i][0],y+b[i][1]);
+			vis[x+b[i][0]][y+b[i][1]]=1;
+			sdfs(x+b[i][0],y+b[i][1]);
+			vis[x+b[i][0]][y+b[i][1]]=0;
+			tail--;
+		}
+	}
+	return;
+}
+void allpath(){
+	system("cls");
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			vis[i][j]=0;
+		}
+	}
+	tail=0;
+	q[tail]=make_pair(1,1);
+	vis[1][1]=1;
+	tail++;
+	sdfs(1,1);
+	system("pause");
+	return;
+}
 void work()
 {
 	system("chcp 65001");
+	//system("chcp 437");
 	MAIN:
 	system("cls");
 	cout<<"Main Menu"<<endl;
 	cout<<"1.Generate maze"<<endl;
 	cout<<"2.Import maze file"<<endl;
 	cout<<"3.Show real-time path"<<endl;
-	cout<<"4.Get all road"<<endl;
-	cout<<"5.Get shortest path"<<endl;
+	cout<<"4.Get shortest path"<<endl;
+	cout<<"5.Get all path"<<endl;
 	cout<<"6.EXIT"<<endl;
-	cout<<"Please input your choice here:";
+	cout<<"Please input your choice here:"<<flush;
 	int opt;
 	cin>>opt;
 	if(opt==1){
@@ -281,9 +367,11 @@ void work()
 		show();
 		goto MAIN;
 	}else if(opt==4){
-
+        srt();
+        goto MAIN;
 	}else if(opt==5){
-		
+        allpath();
+		goto MAIN;
 	}else if(opt==6){
 		return;
 	}
@@ -293,7 +381,7 @@ void work()
 	goto MAIN;
 }
 signed main()
-{
+{   srand(time(0));
 	std::ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	int t = 1;
@@ -302,4 +390,4 @@ signed main()
 		work();
 	}
 	return 0;
-}
+}//学号 19041822 罗汉东

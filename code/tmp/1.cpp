30aafacd9479dcbfd7a4404b3fa3f787d11679e3
randowlh <1303775690@qq.com>
2020-10-19 13:13:15 +0800

    back up

--- code/tmp/1.cpp
+++ code/tmp/1.cpp
@@ -1,22 +1,95 @@
-#include<stdio.h> 
-int main(void)
-{
-	int x,y,z,m,n,a;
-	
-	printf("Enter m:");
-	printf("Enter n:");
-	while(scanf ("%d %d",&m,&n)!=EOF){
-		
-	    for (a=m; a<=n;a++) {
-	    x=a/100; 
-		y=((a%100)/10); 
-		z=((a%100)%10);
-		if (a==x*x*x+y*y*y+z*z*z){
-				printf("%d  ",a);
+#include <bits/stdc++.h>
+using namespace std;
+#pragma optimize(2)
+//#pragma GCC optimize("Ofast,no-stack-protector")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+const int inf = 0x7FFFFFFF;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+template<class T>inline void MAX(T &x,T y){if(y>x)x=y;}
+template<class T>inline void MIN(T &x,T y){if(y<x)x=y;}
+template<class T>inline void rd(T &x){
+   x=0;char o,f=1;
+   while(o=getchar(),o<48)if(o==45)f=-f;
+   do x=(x<<3)+(x<<1)+(o^48);
+   while(o=getchar(),o>47);
+   x*=f;
+}
+template<class T>
+void wt(T x){
+   if(x < 0) putchar('-'), x = -x;
+   if(x >= 10) wt(x / 10);
+   putchar('0' + x % 10);
+}
+#define MP make_pair
+#define pb push_back
+#define pt putchar
+#define yx_queue priority_queue
+#define lson(pos) (pos<<1)
+#define rson(pos) (pos<<1|1)
+#define y1 code_by_Rand0w
+#define yn A_muban_for_ACM
+#define j1 it_is just_an_eastegg
+#define lr hope_you_will_be_happy_to_see_this
+#define int long long
+#define rep(i, a, n) for (register int i = a; i <= n; ++i)
+#define per(i, a, n) for (register int i = n; i >= a; --i)
+const ll llinf = 4223372036854775807;
+const ll mod = (0 ? 1000000007 : 998244353);
+const ll mod2 = 999998639;
+const int m1 = 998244353;
+const int m2 = 1000001011;
+const int pr=233;
+const double eps = 1e-7;
+const int maxm= 1;
+const int maxn = 510000;
+int date[10];
+int ans=0;
+int ck[10];
+void dfs(int step){
+	if(step==7){
+		int a1=date[1]*100+date[2]*10+date[3];
+		int a2=date[4]*100+date[5]*10+date[6];
+		int ta=a1-a2;
+		if(ta<=0)
+			return;
+		set<int> s;
+		for(int i=1;i<=3;i++,s.insert(ta%10),ta/=10)
+			if(ck[ta%10]||s.count(ta%10))
+				return;
+		cout<<a1<<"-"<<a2<<"="<<a1-a2<<endl;
+		ans++;
+		return;
+	}
+	for(int i=1;i<=9;i++){
+		if(!ck[i]){
+			ck[i]=1;
+			date[step]=i;
+			dfs(step+1);
+			ck[i]=0;
 		}
-		else
-	      printf ("no\n");
-	   }
-   }	
-	return 0;
+	}
+}
+void work()
+{
+	ck[0]=1;
+	dfs(1);
+	cout<<"total="<<ans<<endl;
+}
+signed main()
+{
+   #ifndef ONLINE_JUDGE
+   freopen("in.txt","r",stdin);
+//freopen("out.txt","w",stdout);
+#endif
+//std::ios::sync_with_stdio(false);
+//cin.tie(NULL);
+int t = 1;
+//cin>>t;
+while (t--)
+{
+work();
+}
+return 0;
 }
